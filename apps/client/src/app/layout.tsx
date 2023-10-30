ae64ba9619a1501153449371b27c0d5645c4169d
Dmitrii Kopenkin s-dkopenkin@digikare.com <s-dkopenkin@digikare.com>
2023-10-30 03:01:09 +0100

    chore: remove unused metadata

--- apps/client/src/app/layout.tsx
+++ apps/client/src/app/layout.tsx
@@ -14,27 +14,9 @@ const fontSans = Inter({
   variable: "--font-sans",
 });
 
-/**
- * Since we're passing `headers()` to the `TRPCReactProvider` we need to
- * make the entire app dynamic. You can move the `TRPCReactProvider` further
- * down the tree (e.g. /dashboard and onwards) to make part of the app statically rendered.
- */
-export const dynamic = "force-dynamic";
-
 export const metadata: Metadata = {
-  title: "Create T3 Turbo",
-  description: "Simple monorepo with shared backend for web & mobile apps",
-  openGraph: {
-    title: "Create T3 Turbo",
-    description: "Simple monorepo with shared backend for web & mobile apps",
-    url: "https://create-t3-turbo.vercel.app",
-    siteName: "Create T3 Turbo",
-  },
-  twitter: {
-    card: "summary_large_image",
-    site: "@jullerino",
-    creator: "@jullerino",
-  },
+  title: "Starter repo",
+  description: "Simple monorepo with shared backend for web",
 };
 
 export default function Layout(props: { children: React.ReactNode }) {
