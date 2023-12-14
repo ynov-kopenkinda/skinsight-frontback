3eedfcd19f4bbd10da35834b53534695afeb3ff9
Dindault Lucas <dindault.lucas@gmail.com>
2023-12-14 16:01:29 +0100

    Adding header [TMP] and home banner

--- apps/client/src/app/(dashboard)/page.tsx
+++ apps/client/src/app/(dashboard)/page.tsx
@@ -1,5 +1,10 @@
-import { Flex } from "@radix-ui/themes";
+import { Card, Flex } from "@radix-ui/themes";
+import HomeBanner from "./HomeBanner";
 
 export default async function HomePage() {
-  return <Flex direction="column"></Flex>;
+  return (
+    <Flex direction="column">
+      <HomeBanner />
+    </Flex>
+  );
 }
--- tooling/tailwind/index.ts
+++ tooling/tailwind/index.ts
@@ -1,9 +1,20 @@
 import type { Config } from "tailwindcss";
 
+import defaultTheme from "tailwindcss/defaultTheme";
+
 export default {
   content: [""],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'primary': '#5861E2',
+        'secondary': '#D1F6DB',
+      },
+      fontFamily: {
+        'sans': ['"Inter"', ...defaultTheme.fontFamily.sans],
+        'inter': ['"Inter"', ...defaultTheme.fontFamily.sans]
+      }
+    },
   },
   plugins: [],
 } satisfies Config;
