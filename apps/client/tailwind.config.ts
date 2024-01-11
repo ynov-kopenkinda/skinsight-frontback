0c924035eed9d3efe352a0e52d42f0267b5c6332
Dindault Lucas <dindault.lucas@gmail.com>
2024-01-11 12:14:21 +0100

    add tailwind color config into client

--- apps/client/tailwind.config.ts
+++ apps/client/tailwind.config.ts
@@ -4,5 +4,15 @@ import baseConfig from "@skinsight/tailwind-config";
 
 export default {
   content: ["./src/**/*.{ts,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        'primary': '#5861E2',
+        'secondary': '#D1F6DB',
+        'gray': '#F2F2F7',
+        'gray-strong': '#8A8A8E',
+      },
+    },
+  },
   presets: [baseConfig],
 } satisfies Config;
