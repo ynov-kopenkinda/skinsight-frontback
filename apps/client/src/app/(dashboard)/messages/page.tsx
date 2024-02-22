9e04ac5562d8dca2306c05a5a16691ad2e1d7d90
Dindault Lucas <dindault.lucas@gmail.com>
2024-02-22 16:36:02 +0100

    get last message from db on front

--- apps/client/src/app/(dashboard)/messages/page.tsx
+++ apps/client/src/app/(dashboard)/messages/page.tsx
@@ -2,54 +2,49 @@
 
 import React, { useState } from "react";
 
-import type { MessageBoxProps } from "~/shared/ui/MessageBox";
 import MessageBox from "~/shared/ui/MessageBox";
 import TapBar from "../components/TapBar";
 import HeaderMessage from "./components/header-message";
 import Searchbar from "./components/searchbar";
+import { api } from "~/utils/api/react";
 
 function Messages() {
-  const messages: MessageBoxProps[] = [
-    {
-      id: 1,
-      avatar: "https://api.dicebear.com/7.x/initials/svg",
-      name: "Anna",
-      lastMessageContent: "You have a good heart!",
-      lastMessageTime: "12:45",
-      link: "/",
-    },
-    {
-      id: 2,
-      name: "Hendra",
-      lastMessageContent: "How are you today?",
-      lastMessageTime: "12:45",
-      link: "/",
-    },
-  ];
+  // TODO change by user ID
+  // const { data } = useSession();
+
+  const messages = api.chat.getChatsByUserId.useQuery({ id: 1 });
 
   const [searchTerm, setSearchTerm] = useState("");
 
-  const filteredMessage = messages.filter((message) =>
-    message.name.toLowerCase().includes(searchTerm.toLowerCase()),
+  const filteredMessage = messages.data?.filter((message) =>
+    message.firstname.toLowerCase().includes(searchTerm.toLowerCase()),
   );
 
   return (
     <div>
       <HeaderMessage />
       <Searchbar onSearch={setSearchTerm} />
-      {filteredMessage.length > 0
-        ? filteredMessage.map((message) => (
-            <MessageBox
-              key={message.id}
-              id={message.id}
-              avatar={message.avatar}
-              name={message.name}
-              lastMessageContent={message.lastMessageContent}
-              lastMessageTime={message.lastMessageTime}
-              link={message.link}
-            />
-          ))
+      {filteredMessage && filteredMessage.length > 0
+        ? filteredMessage.map((message) => {
+            const lastMessageDate = new Date(message.lastMessageDate);
+            const timeFormatted = new Intl.DateTimeFormat("fr-FR", {
+              hour: "2-digit",
+              minute: "2-digit",
+              hour12: false,
+            }).format(lastMessageDate);
+            return (
+              <MessageBox
+                key={message.id}
+                id={message.id}
+                name={message.firstname}
+                lastMessageContent={message.lastMessage}
+                lastMessageTime={timeFormatted}
+                link={`/messages/${message.id}`}
+              />
+            );
+          })
         : "You don't have any messages for the moment."}
+
       <div className="border-gray glassmorphism container fixed bottom-4 left-0 right-0 z-50 mx-auto block w-fit rounded-xl border-2 bg-white pl-4 pr-4">
         <TapBar />
       </div>
