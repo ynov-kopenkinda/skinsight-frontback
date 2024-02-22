3bd25479fb77d3657a98ac4f7ff39721ccd01032
Dindault Lucas <dindault.lucas@gmail.com>
2024-02-22 12:29:06 +0100

    Add chat router & nextauth provider

---
+++ packages/api/src/router/chat.ts
@@ -0,0 +1,10 @@
+import { z } from "zod";
+import { createTRPCRouter, publicProcedure } from "../trpc";
+
+export const chatRouter = createTRPCRouter({
+  getChatsByUserId: publicProcedure.input(z.object({
+    id: z.number(),
+  })).query(({ input, ctx }) => {
+    return ctx.nest.chat.chatControllerGetChatByUserId(input)
+  })
+})
