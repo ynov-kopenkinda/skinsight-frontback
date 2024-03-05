54b7c9470188db93decf53503dd12ee2b6f6bb45
Dindault Lucas <dindault.lucas@gmail.com>
2024-03-05 09:46:06 +0100

    Fix smthing

--- apps/api-server/src/chat/chat.controller.ts
+++ apps/api-server/src/chat/chat.controller.ts
@@ -16,35 +16,35 @@ import {
 
 import { ChatService } from "./chat.service";
 import { CreateChatDto } from "./dto/create-chat.dto";
-import { Chat } from "./entity/chat.entity";
+import { ChatDto } from "./entity/chat.entity";
 
 @ApiTags("Chat")
 @Controller("chat")
 export class ChatController {
   constructor(private readonly chatService: ChatService) {}
 
-  @ApiOkResponse({ type: Chat })
+  @ApiOkResponse({ type: ChatDto })
   @ApiOperation({ summary: "Get a chat depending on passed id" })
   @Get(":id")
   async getChat(@Param("id", ParseIntPipe) id: number) {
     return this.chatService.getChatById(id);
   }
 
-  @ApiOkResponse({ type: Chat })
+  @ApiOkResponse({ type: ChatDto })
   @ApiOperation({ summary: "Create a chat depending on passed id" })
   @Post()
   async createChat(@Body() body: CreateChatDto) {
     return this.chatService.createChat(body);
   }
 
-  @ApiResponse({ type: [Chat] })
+  @ApiResponse({ type: [ChatDto] })
   @ApiOperation({ summary: "Get chat(s) where an user appear" })
   @Get("/user/:id")
   async getChatByUserId(@Param("id", ParseIntPipe) id: number) {
     return this.chatService.getChatByUserId(id);
   }
 
-  @ApiOkResponse({ type: Chat })
+  @ApiOkResponse({ type: ChatDto })
   @ApiOperation({ summary: "Delete a chat depending on passed id" })
   @Delete(":id")
   async deleteChat(@Param("id", ParseIntPipe) id: number) {
