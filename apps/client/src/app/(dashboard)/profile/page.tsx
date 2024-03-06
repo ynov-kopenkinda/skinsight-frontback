db227eb60fd68dc3121bf43b1333e63b612cb721
Dindault Lucas <dindault.lucas@gmail.com>
2024-03-06 15:35:52 +0100

    end integration /profile page

--- apps/client/src/app/(dashboard)/profile/page.tsx
+++ apps/client/src/app/(dashboard)/profile/page.tsx
@@ -3,6 +3,8 @@
 import Image from 'next/image';
 import React from 'react';
 import { api } from '~/utils/api/react';
+import BoxOptions from './components/box-options';
+import { IconDots, IconLock, IconLogout, IconQuestionMark, IconShield, IconUser } from '@tabler/icons-react';
 
 const Profile = () => {
   const user = api.user.getUserById.useQuery({ id: 1 });
@@ -22,6 +24,19 @@ const Profile = () => {
             <p className='text-lg font-semibold'>{user.data.lastName} {user.data.firstName}</p>
             <p className='font-light'>{user.data.email}</p>
           </div>
+        </div>
+
+        <div className="flex flex-col pt-4 pb-4 shadow-lg border border-slate-100 rounded-xl mt-8">
+          <h2 className='text-gray-strong pl-4 text-xl font-medium pb-2'>General</h2>
+          <BoxOptions icon={<IconUser width={24} />} title='Edit profile' subtitle='Change your personal information' link='/profile/edit' />
+          <BoxOptions icon={<IconLock width={24} />} title='Change password' subtitle='Update and strengthen account security' link='/profile/change-password' />
+          <BoxOptions icon={<IconShield width={24} />} title='Terms of use' subtitle='All informations about your datas.' link='/profile/terms-of-use' />
+        </div>
+
+        <div className="flex flex-col pt-4 pb-4 shadow-lg border border-slate-100 rounded-xl mt-8">
+          <h2 className='text-gray-strong pl-4 text-xl font-medium pb-2'>Preferences</h2>
+          <BoxOptions icon={<IconQuestionMark width={24} />} title='FAQ' subtitle='Any questions you may have' link='/faq' />
+          <BoxOptions icon={<IconLogout width={24} />} title='Logout' subtitle='Disconnect from your account' link='/logout' isLogOutButton={true} />
         </div>
       </div>
     );
--- apps/client/src/shared/ui/IconButton.tsx
+++ apps/client/src/shared/ui/IconButton.tsx
@@ -9,6 +9,7 @@ interface IconButtonProps {
   extraStyle?: string;
   icon: ReactNode;
   link: string;
+  iconFullRounded?: boolean;
 }
 
 const IconButton = ({
@@ -18,18 +19,13 @@ const IconButton = ({
   extraStyle,
   icon,
   link,
+  iconFullRounded
 }: IconButtonProps) => {
   return (
     <div className="group">
       <Link href={link} className="flex flex-col items-center space-y-2">
-        <div
-          className={`${
-            background ?? "bg-gray"
-          } flex h-12 w-12 flex-col items-center justify-center rounded-lg ${
-            extraStyle ? extraStyle : ""
-          }`}
-        >
-          <span className={`rounded-lg p-2 ${color ?? "text-primary"}`}>
+        <div className={`${background ?? "bg-gray"} ${extraStyle ? extraStyle : ""} flex h-12 w-12 flex-col items-center justify-center ${iconFullRounded ? 'rounded-full' : 'rounded-lg'}`}>
+          <span className={`${iconFullRounded ? 'rounded-full' : 'rounded-lg'} p-2 ${color ?? "text-primary"}`}>
             {icon}
           </span>
         </div>
