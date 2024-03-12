54993fabab7cf98e5224f34bb58a25025261ccf9
GuyMickael <mickael.guy.pro@gmail.com>
2024-03-12 16:02:44 +0100

    Upload images

--- apps/api-server/src/pre-appointment/pre-appointment.service.ts
+++ apps/api-server/src/pre-appointment/pre-appointment.service.ts
@@ -1,12 +1,32 @@
+import crypto from "crypto";
+import { PutObjectCommand } from "@aws-sdk/client-s3";
 import { Injectable } from "@nestjs/common";
 import { PrismaService } from "src/prisma/prisma.service";
+import { s3 } from "src/s3";
 
 @Injectable()
 export class PreAppointmentService {
   constructor(private readonly prisma: PrismaService) {}
-  async create(createPreAppointmentDto) {
+  async create(createPreAppointmentDto, image: Express.Multer.File) {
+    const random_image_name = crypto.randomBytes(32).toString("hex");
+
+    const putParams = {
+      Bucket: process.env.S3_BUCKET_NAME,
+      Key: random_image_name,
+      Body: image.buffer,
+      ContentType: image.mimetype,
+    };
+
+    const putCommand = new PutObjectCommand(putParams);
+
+    const returnedImage = await s3.send(putCommand).catch((err) => {
+      throw new Error(err);
+    });
+
+    console.log(returnedImage, "returnedImage");
+
     return this.prisma.preAppointment.create({
-      data: { ...createPreAppointmentDto },
+      data: { ...createPreAppointmentDto, image: random_image_name },
     });
   }
 
